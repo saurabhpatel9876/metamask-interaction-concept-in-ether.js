7bfcd7fed3cb5050f520800af509d55d8b4f9184
saurabh <ironman27012004@gmail.com>
2023-09-04 20:57:17 +0530

    metamask usecase

--- src/App.js
+++ src/App.js
@@ -1,22 +1,88 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState, useEffect } from "react";
+import { ethers} from "ethers";
+import detectEthereumProvider from '@metamask/detect-provider';
 
 function App() {
+  const { ethereum } = window;
+  const [address, setAddress] = useState("connect wallet.... ");
+  const [balance, setBalance] = useState("");
+  // we will use useeffect here for account, change chainid
+  useEffect(()=>{
+    ethereum.on("accountsChanged",(accouts)=>{
+      setAddress(accouts[0])
+      const getbal = async()=>{
+        const balance = await ethereum.request({
+          method: "eth_getBalance",
+          params: [accouts[0], "latest"],
+        });
+        setBalance(ethers.formatEther(balance))
+      }
+      getbal();
+    })
+    ethereum.on("chainChanged",(chain)=>{
+      console.log(chain)
+    })
+  })
+  // for giving option of changing network in my dapp 
+  // const changeChain = async()=>{
+  //   await ethereum.request({method:"wallet_addEthereumChain",
+  // params:[
+  //   {}
+  // ]})
+  // }
+  // const switchChain = async()=>{
+  //   await ethereum.request({method:"wallet_switchEthereumChain",
+  // params:[
+  //   {chainId:`0x13881`}
+  // ]})
+  // }
+  // TRANSACTION IN METAMASK 
+  const sendTx = async()=>{
+    await ethereum.request({method:"eth_sendTransaction",
+  params:[{
+    to: '0x68FF53709d5Ebe539a746D892C552E6Fd61c7860' ,
+    from: address,
+    // value:'0xB1A2BC2EC50000', // here value is in hexadecimal
+    value:`0x${(parseInt(ethers.parseEther('0.05'))).toString(16)}`,
+    chaiId:'0xaa36a7',
+  }]})
+  }
+
+  const requestAccount = async () => {
+    // await ethereum.request({
+    //   method: "wallet_requestPermissions",
+    //   params: [
+    //     {
+    //       eth_accounts: {},
+    //     },
+    //   ],
+    // });  ////it will give as array so used accounts[] and it will be for wallet permission
+  // if we will comment out this wallet_permsis then we have to not connect agian and again
+    const accouts = await ethereum.request({
+      method: "eth_requestAccounts",
+    });
+    console.log(accouts); /// now it's for account request
+    setAddress(accouts[0]);
+
+    // balance check
+    const balance = await ethereum.request({
+      method: "eth_getBalance",
+      params: [accouts[0], "latest"],
+    }); // --> it will be in hexadecimal
+    setBalance(ethers.formatEther(balance)) // here when i used utils then it shows error
+  };
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button className="App-link" onClick={requestAccount}>
+          {address}
+        </button>
+        <a>{balance}</a>
+        {/* <button onClick={switchChain}>
+           switch
+        </button> */}
+        <button onClick={sendTx}>transaction</button>
       </header>
     </div>
   );
